ed7045a0d21052f683994518c4d83e19533c58a0
hamiha <haiminh.utc@gmail.com>
2017-04-27 21:13:49 -0400

    update Result

--- scripts.js
+++ scripts.js
@@ -1,11 +1,37 @@
+function result(goal1, goal2){
+	if(goal1 > goal2)
+		return 0;
+	else 
+		if (goal1 < goal2) return 2;
+	else return 1;
+}
+
 function processData(errors, data) {
 
+	// console.log(data);
 	var matrix = [];
 
 	_.each(data, function(d) {
 
+		// calculate goal for each team
+		goal = d["FT"].split("-");
+		goal1 = +goal[0];
+		goal2 = +goal[1];
+
+		// calculate form
+		
+
+		// add to object
+		var sample = {
+			"team1": d["Team 1"],
+			"team2": d["Team 2"],
+			"result": result(goal1, goal2) / 2
+		}
+		// console.log(goal1, goal2);
+		matrix.push(sample);
 	});
 
+	// console.log(matrix);
 }
 
 d3.queue()
