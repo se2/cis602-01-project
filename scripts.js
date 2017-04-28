4117f07516c502aac48de093594fc3317b9cbbaa
hamiha <haiminh.utc@gmail.com>
2017-04-27 23:41:16 -0400

    add history, diff features

--- scripts.js
+++ scripts.js
@@ -1,37 +1,86 @@
-function result(goal1, goal2){
+
+function getHistoryMatch(data, date, match){
+	newDate = moment(date);
+	
+	filterByDate =	_.filter(data, function(d){
+		if(moment(d["Date"]).isBefore(newDate)){
+				return d;	
+		}
+	})
+
+	filterByMatch = _.filter(filterByDate, function(d){
+		submatch = generateMatch(d["Team 1"], d["Team 2"]);
+		if (submatch===match) {
+			return d;
+		}
+	})
+
+	return filterByMatch;
+}
+
+function generateMatch(team1, team2){
+	var match = [];
+	match.push(team1, team2);
+	return match.sort().toString();
+}
+
+function different(match){
+	if (match.length) {
+		score = (match[0].FT).split("-");
+		// console.log(match);
+		goal1 = +score[0];
+		goal2 = +score[1];
+		
+		return Math.abs(goal1-goal2) / 10;
+	}
+	else return 0;
+}
+
+function result(score){
+	score = score.split("-");
+	goal1 = +score[0];
+	goal2 = +score[1];
+	
+	res = 0;
+	
 	if(goal1 > goal2)
-		return 0;
+		res = 0;
 	else 
-		if (goal1 < goal2) return 2;
-	else return 1;
+		if (goal1 < goal2) res = 2;
+	else res = 1;
+
+	return res / 2;
+}
+
+function history(match){
+	if (match.length) {
+		return result(match[0].FT);
+	}
+	else return 0;
 }
 
 function processData(errors, data) {
-
 	// console.log(data);
 	var matrix = [];
 
 	_.each(data, function(d) {
 
-		// calculate goal for each team
-		goal = d["FT"].split("-");
-		goal1 = +goal[0];
-		goal2 = +goal[1];
-
-		// calculate form
+		match = generateMatch(d["Team 1"], d["Team 2"]);
+		// console.log(match);
+		historyMatch = getHistoryMatch(data, d["Date"], match);
 		
-
 		// add to object
 		var sample = {
 			"team1": d["Team 1"],
 			"team2": d["Team 2"],
-			"result": result(goal1, goal2) / 2
+			"history": history(historyMatch),
+			"dif": different(historyMatch)
 		}
 		// console.log(goal1, goal2);
 		matrix.push(sample);
 	});
 
-	// console.log(matrix);
+	console.log(matrix);
 }
 
 d3.queue()
